bc0327096e277a50bde2dd0482b71dfd10690002
Colm Duffy <cduffy@transperfect.com>
2020-03-31 13:52:31 +0100

    updated to use only one project.

--- Elsa.AM.Issue/Startup.cs
+++ Elsa.AM.Issue/Startup.cs
@@ -1,7 +1,13 @@
 using AutoMapper;
-using Elsa.AM.WorkflowCodebase.DependencyInjection;
+using Elsa.Activities;
+using Elsa.Activities.Http.Activities;
+using Elsa.Activities.Http.Extensions;
+using Elsa.Dashboard.Extensions;
+using Elsa.Persistence.EntityFrameworkCore.DbContexts;
+using Elsa.Persistence.EntityFrameworkCore.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,9 +26,28 @@ namespace Elsa.AM.Issue
 
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddAutoMapper(typeof(EM.AM.Issue.Profiles.ClassA).Assembly);
+			services.AddAutoMapper(opt => opt.Advanced.AllowAdditiveTypeMapCreation = true, typeof(Startup).Assembly);
 
-			services.AddWorkflowConfiguration(() => Configuration.GetConnectionString("workflowDataSource"));
+			//services.AddWorkflowConfiguration(() => );
+
+			services.AddElsa(elsa =>
+			{
+				elsa.AddEntityFrameworkStores<SqlServerContext>(options => options.UseSqlServer(Configuration.GetConnectionString("workflowDataSource")));
+			}).AddHttpActivities()
+			.AddElsaDashboard(
+			options =>
+			{
+				options.Configure(x =>
+				{
+					/*
+					 * If I do not add these two lines then I do not get the Activities registered on the Dashboard. See Ref #260 - https://github.com/elsa-workflows/elsa-core/issues/260
+					 */
+					x.ActivityDefinitions.Discover(t => t.FromAssembliesOf(typeof(SetVariable)));
+					x.ActivityDefinitions.Discover(t => t.FromAssemblies(typeof(ReceiveHttpRequest).Assembly));
+				});
+			});
+
+
 			services.AddMvc(config => config.EnableEndpointRouting = false)
 				.AddNewtonsoftJson(a => a.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter(new Newtonsoft.Json.Serialization.CamelCaseNamingStrategy(), true)));
 		}
@@ -34,7 +59,14 @@ namespace Elsa.AM.Issue
 				app.UseDeveloperExceptionPage();
 				autoMapper.AssertConfigurationIsValid();
 			}
-			app.UseWorkflowConfiguration();
+
+			app.UseHttpActivities();
+
+			using var scope = app.ApplicationServices.CreateScope();
+			var scopeProvider = scope.ServiceProvider;
+			var context = scopeProvider.GetRequiredService<ElsaContext>();
+
+			context.Database.Migrate();
 
 			app.UseStaticFiles()
 			.UseMvc(routes =>
